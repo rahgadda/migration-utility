f36db350929e53e348bcf10de81ffa8a64a4e992
rahgadda <gaddam.rahul@gmail.com>
2023-09-11 05:30:03 +0000

    Initial Draft

--- code/Dashboard.py
+++ code/Dashboard.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import lib.load_hf_dataset as load_hf_dataset
 from datasets import load_dataset
 import os
 
@@ -12,28 +13,25 @@ DATA_SET = os.environ.get("DATA_SET")
 ################################
 ####### GenericFunctions #######
 ################################
-def load_hf_dataset():
-    dataset = load_dataset(DATA_SET)
-    return dataset
     
 def save_dataset():
     # Load a dataset from the Hugging Face Hub
     dataset = load_hf_dataset()
 
     # Create a directory to save the files
-    output_directory = "data"
-    os.makedirs(output_directory, exist_ok=True)
+    # output_directory = "data"
+    # os.makedirs(output_directory, exist_ok=True)
 
     # Iterate through the dataset and save each file to the data folder
-    for split in dataset.keys():
-        for file_name, file_content in enumerate(dataset[split]):
-            file_path = os.path.join(output_directory, file_name)    
-            with open(file_path, 'w', encoding='utf-8') as file:
-                file.write(file_content)
+    # for split in dataset.keys():
+    #     for file_name, file_content in enumerate(dataset[split]):
+    #         file_path = os.path.join(output_directory, file_name)    
+    #         with open(file_path, 'w', encoding='utf-8') as file:
+    #             file.write(file_content)
     
-            print(f"Saved: {file_path}")
+    #         print(f"Saved: {file_path}")
 
-    print("All files saved successfully.")
+    # print("All files saved successfully.")
 
     
 ################################
---
+++ code/lib/test/test_load_hf_dataset.py
@@ -0,0 +1,10 @@
+import unittest
+from .. import load_hf_dataset
+
+class TestModuleA(unittest.TestCase):
+
+    def test_greet(self):
+        self.assertEqual(a.greet("Alice"), "Hello, Alice!")
+
+if __name__ == '__main__':
+    unittest.main()
